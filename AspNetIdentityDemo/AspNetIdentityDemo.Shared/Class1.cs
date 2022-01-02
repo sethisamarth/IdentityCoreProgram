a6d49970976f7bd20197f05cc15448d5313063b7
Samarth <sethisamarth1@gmail.com>
2022-01-02 14:52:26 +0530

    Registration Done

---
+++ AspNetIdentityDemo/AspNetIdentityDemo.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AspNetIdentityDemo.Shared
+{
+    public class Class1
+    {
+    }
+}
---
+++ AspNetIdentityDemo/AspNetIdentityDemoApi/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetIdentityDemoApi.Models
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+}
